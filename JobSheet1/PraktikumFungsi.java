2f0f494215ee4aa7c2678def10b07be84e37d641
TomiMartinoAffandi <tomimartino10@gmail.com>
2024-02-18 09:30:32 +0700

    percobaan 4

--- JobSheet1/PraktikumFungsi.java
+++ JobSheet1/PraktikumFungsi.java
@@ -1,24 +1,40 @@
 
 public class PraktikumFungsi {
+    public static void main(String[] args) {
+        String pemisah = "=============================================================";
+        System.out.printf("%-20s", "Cabang Toko");
+        for (int i = 0; i < namaBunga.length; i++) {
+            System.out.print("\t" + namaBunga[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < namaToko.length; i++) {
+            System.out.printf("%-20s", namaToko[i]);
+            for (int j = 0; j < stokToko.length; j++) {
+                System.out.print("\t" + stokToko[i][j]+ "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("harga tiap bunga: ");
+        for (int i = 0; i < namaBunga.length; i++) {
+           System.out.print(namaBunga[i]+": "+harga[i]+"/pcs\t");
+        }
+        System.out.println();
+        System.out.println(pemisah);
+        pendapatanPerCabang();
+        System.out.println(pemisah);
+        System.out.println("Stock Cabang Royal Garden Setelah Pengurangan: ");
+        stockBaru4();
+    }
     static String[] namaBunga = {"Aglonema", "Keladi  ", "Alocasia", "Mawar   "};
-    static String namaToko[] = {
-        "Royal Garden 1", "Royal Garden 2", "Royal Garden 3", "Royal Garden 4"
-    };
-        static int stokToko[][] = {
-        { 10, 5, 15, 7 },
-        { 6, 11, 11, 12 },
-        { 2, 10, 10, 5 },
-        { 5, 7, 12, 9 }
-    };
-    static int harga[] = {
-        75000, 50000, 60000, 10000
-    };
-    static void stockCabang4() {
+    static String namaToko[] = {"Royal Garden 1", "Royal Garden 2", "Royal Garden 3", "Royal Garden 4"};
+    static int stokToko[][] = {{ 10, 5, 15, 7 }, { 6, 11, 11, 12 }, { 2, 10, 10, 5 },{ 5, 7, 12, 9 }};
+    static int harga[] = {75000, 50000, 60000, 10000};
+    static void stockBaru4() {
         int[] bungaMati = {1, 2, 0, 5};
-        int[] newStock = new int[4];
+        int[] StockBaru = new int[4];
         for (int i = 0; i < stokToko.length; i++) {
-            newStock[i] = stokToko[3][i] - bungaMati[i];
-            System.out.println(namaBunga[i] + "        : " + newStock[i]);
+            StockBaru[i] = stokToko[3][i] - bungaMati[i];
+            System.out.println(namaBunga[i] + ": " + StockBaru[i]);
         }
     }
     static void pendapatanPerCabang() {
@@ -30,22 +46,5 @@ public class PraktikumFungsi {
             }
             System.out.println("Royal Garden "+(i+1)+": "+pendapatan[i]);
         }
-    }
-    public static void main(String[] args) {
-        String pemisah = "=============================================================";
-        System.out.printf("%-20s%-15s%-15s%-15s%-15s\n", "Nama Cabang", "Aglonema", "Keladi", "Alcosia", "Mawar");
-            for (int i = 0; i < stokToko.length; i++) {
-                System.out.printf("%-20s", namaToko[i]);
-                for (int j = 0; j < stokToko.length; j++) {
-                    System.out.printf("%-15s", stokToko[i][j]);
-                }
-                System.out.println();
-            }
-        System.out.println("Harga tiap - tiap bunga :\nAlgonema: 75000/pcs,\tKeladi: 50000/pcs,\tAlcosia: 60000/pcs,\tMawar: 10000/pcs");
-        System.out.println(pemisah);
-        pendapatanPerCabang();
-        System.out.println(pemisah);
-        System.out.println("Stock Cabang Royal Garden Setelah Pengurangan: ");
-        stockCabang4();
     }
 }
