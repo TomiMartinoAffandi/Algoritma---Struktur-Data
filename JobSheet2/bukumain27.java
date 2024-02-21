d8f43b2d057f78d1844c35330a4a6d01b4b3fc6b
TomiMartinoAffandi <tomimartino10@gmail.com>
2024-02-21 19:43:43 +0700

    modify soal no 5

--- JobSheet2/bukumain27.java
+++ JobSheet2/bukumain27.java
@@ -1,7 +1,7 @@
 package JobSheet2;
 public class bukumain27 {
     public static void main(String[] args) {
-        buku27 bk1 = new buku27();
+        buku27 bk1 = new buku27();  
         bk1.judul = "Today Ends Tomorrow Comes";
         bk1.pengarang = "Denanda Pratiwi";
         bk1.halaman = 198;
@@ -16,5 +16,9 @@ public class bukumain27 {
         buku27 bk2 = new buku27("Self reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
+
+        buku27 bukuTomi = new buku27("Hanya Pria Biasa", "Tomi Martino Affandi", 500, 100, 200000);
+        bukuTomi.terjual(50);
+        bukuTomi.tampilInformasi();
     }
 }
