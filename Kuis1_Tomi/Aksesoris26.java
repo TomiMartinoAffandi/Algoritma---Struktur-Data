62a2fd6dcaa7d7a8836a443f0606132b35683f23
Your Name <tomimartino10@gmail.com>
2024-03-09 14:45:30 +0700

    quiz

---
+++ Kuis1_Tomi/Aksesoris26.java
@@ -0,0 +1,42 @@
+
+public class Aksesoris26 {
+    String nama26;
+    String kategori26;
+    int harga26;
+    int terjual26;
+    
+    public Aksesoris26(){
+
+    }
+
+    public Aksesoris26(String nama,String kategori,int harga,int terjual){
+        this.nama26 = nama;
+        this.kategori26 = kategori;
+        this.harga26 = harga;
+        this.terjual26 = terjual;
+    }
+
+    public void tampil(){
+        System.out.print("Nama Aksesoris: "+nama26+"\n");
+        System.out.print("Kategori: "+kategori26+"\n");
+        System.out.print("Harga: "+harga26+"\n");
+        System.out.print("Terjual: "+terjual26+"\n");
+        
+    }
+
+    public int hitungTotalPenjualan(){
+        int totalPenjualan;
+        return totalPenjualan = harga26*terjual26;
+    }
+
+    // public void cariAksesorisTermurah(int[] hargaTerendah){
+    //     hargaTerendah;
+    //     if (hargaTerendah == 5000) {
+    //         tampil();
+    //     }
+    // }
+
+    public void daftarAksesoris(){
+        
+    }
+}
---
+++ Kuis1_Tomi/Aksesoris26main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Aksesoris26main {
+    public static void main(String[] args) {
+        Aksesoris26 [] aksArray = new Aksesoris26[5];
+        Scanner sc = new Scanner(System.in);
+        
+        aksArray[0] = new Aksesoris26("Gelang", "Perhiasan", 10000, 10);
+        aksArray[1] = new Aksesoris26("Anting Emas", "Perhiasan", 100000, 3);
+        aksArray[2] = new Aksesoris26("Anting Biasa", "Perhiasan", 5000, 5);
+        aksArray[3] = new Aksesoris26("Kalung Emas", "Perhiasan", 200000, 10);
+        aksArray[4] = new Aksesoris26("Kalung Biasa", "Perhiasan", 20000, 15);
+        
+        for (int i = 0; i < aksArray.length; i++) {
+            aksArray[i].tampil();
+            System.out.println("Penghasilan Aksesoris ke-"+ i + ": " +aksArray[i].hitungTotalPenjualan());
+            // aksArray[i].cariAksesorisTermurah(aksArray[i]);
+        }
+    }
+}
