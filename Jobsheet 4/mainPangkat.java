1afa442e74a765e2844f8d913ce5115a99e50619
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-03-16 17:39:17 +0700

    percobaan 2 pertanyaan 4

--- Jobsheet 4/mainPangkat.java
+++ Jobsheet 4/mainPangkat.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import JobSheet3.balok;
 public class mainPangkat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,26 +16,33 @@ public class mainPangkat {
             System.out.print("Masukan nilai pemangkat: ");
             png[i].pangkat = sc.nextInt();
         }
-        System.out.println("Pilih Metode: \n1. Brute Force\n2. Divided and Conquer");
-        System.out.print("Input: ");
-        int Pilih = sc.nextInt(); 
 
-        switch (Pilih) {
-            case 1:
-            System.out.println("HASIL PANGKAT - BRUTE FORCE: ");
-            for (int i = 0; i < elemen; i++) {
-                System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah: " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
-            }   
-            break;
-        
-            case 2:
-            System.out.println("HASIL PANGKAT - DIVIDED and CONQUER");
-            for (int i = 0; i < png.length; i++) {
-                System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah: " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
-                
+        while (true) {
+            System.out.println("Pilih Metode: \n1. Brute Force\n2. Divided and Conquer\n3. Exit");
+            System.out.print("Input: ");
+            int Pilih = sc.nextInt(); 
+            switch (Pilih) {
+                case 1:
+                System.out.println("HASIL PANGKAT - BRUTE FORCE: ");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah: " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;          
+                case 2:
+                System.out.println("HASIL PANGKAT - DIVIDED and CONQUER");
+                for (int i = 0; i < png.length; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah: " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                    
+                }
+                break;
+                case 3:
+                System.out.println("anda telah keluar");
+                System.exit(Pilih);
+                default:
+                System.out.println("input salah");
             }
-            break;
         }
+        
 
         
 
