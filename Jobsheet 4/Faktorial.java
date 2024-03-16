a380a5ccb1b58793c715e5d8cd442de296aaf11f
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-03-16 13:56:40 +0700

    menggunakan while

--- Jobsheet 4/Faktorial.java
+++ Jobsheet 4/Faktorial.java
@@ -3,11 +3,17 @@ public class Faktorial {
 
     public int faktorialBF(int n){
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto*i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
+    // for (int i = 1; i <= n; i++) {
+    //     fakto = fakto*i;
+    // }
+    // return fakto;
     
     public int faktorialDC(int n){
         if (n==1) {
