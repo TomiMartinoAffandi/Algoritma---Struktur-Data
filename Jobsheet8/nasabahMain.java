2a6f669b918f02ac1b7f5f4c913c717229aab17a
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-04-26 12:51:12 +0700

    percobaan 2

---
+++ Jobsheet8/nasabahMain.java
@@ -0,0 +1,63 @@
+package Jobsheet8;
+import java.util.Scanner;
+public class nasabahMain {
+    public static void menu() {
+        System.out.println("1. Antrian baru");
+        System.out.println("2. Antrian keluar");
+        System.out.println("3. Cek antrian terdepan");
+        System.out.println("4. Cek semua antrian");
+        System.out.println("5. Exit");
+        System.out.println("-----------------------------");
+        System.out.print("pilih menu: ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukan kapasitas queue: ");
+        int jumlah = sc.nextInt();
+        Nasabah antri = new Nasabah(jumlah);
+
+        do {
+            menu();
+            int pilih = sc.nextInt();
+            sc.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("No. rekening: ");
+                    String norek = sc.nextLine();
+                    System.out.print("Nama: ");
+                    String nama = sc.nextLine();
+                    System.out.print("Alamat: ");
+                    String alamat = sc.nextLine();
+                    System.out.print("Umur: ");
+                    int umur = sc.nextInt();
+                    System.out.print("Saldo: ");
+                    double saldo = sc.nextDouble();
+                    sc.nextLine();
+                    Nasabah nb = new Nasabah(norek, nama, alamat, umur, saldo);
+                    antri.enqueue(nb);
+                    break;
+                case 2:
+                    Nasabah data = antri.dequeue();
+                    if (!"".equals(data.norek)&&!"".equals(data.nama)&&!"".equals(data.alamat)&& data.umur != 0 && data.saldo != 0) {
+                        System.out.println("Antrian yang keluar: "+data.norek+""+data.nama+""+data.alamat+""+data.umur+""+data.saldo+"");
+                        break;
+                    }
+                case 3:
+                    antri.peek();
+                    break;
+                case 4:
+                    antri.print();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("input salah");
+                    break;
+            }
+            
+        } while (true);
+    }
+}
