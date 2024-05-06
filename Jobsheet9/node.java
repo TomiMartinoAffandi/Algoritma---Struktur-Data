cd6d97c495cdab5bb4028798a4f1f234a46831a0
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-05-06 16:18:31 +0700

    percobaan 1

---
+++ Jobsheet9/node.java
@@ -0,0 +1,15 @@
+package Jobsheet9;
+
+public class node {
+    int data;
+    node next;
+
+    public node(){
+        
+    }
+
+    public node(int data, node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
