f7df1fe5fc7c80ba94cfc95e2a3d44f3227ef5af
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-05-16 18:40:47 +0700

    Percobaan 2

--- Jobsheet10/DLLMain.java
+++ Jobsheet10/DLLMain.java
@@ -23,5 +23,37 @@ public class DLLMain {
         dll.clear();
         dll.print();
         System.out.println("Size: "+dll.size());
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("====================================");
+        try {
+            dll.removeFirst();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("====================================");
+        try {
+            dll.removeLast();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("====================================");
+        try {
+            dll.remove(1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("====================================");
+        
     }
 }
--- Jobsheet10/DoubleLinkListed.java
+++ Jobsheet10/DoubleLinkListed.java
@@ -65,6 +65,60 @@ public class DoubleLinkListed {
         size++;
     }
 
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("linked list kosong, tidak dapat menghapus");
+        }else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+    
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("linked list kosong, tidak dapat menghapus");
+        }else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        } 
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+    
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("index diluar batas");
+        }else if (index == 0) {
+            removeFirst();
+        }else{
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            }else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            }else{
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
+
     public int size(){
         return size;
     }
